05a141313bd4d8e0921dca70817762dee51a54ca
AndersonSilvaJob <83041378+AndersonSilvaJob@users.noreply.github.com>
2022-03-28 14:09:55 -0300

    Add files via upload

---
+++ VendasDeIngresso/src/VendaDeIngresso.java
@@ -0,0 +1,30 @@
+public class VendaDeIngresso {
+
+    private double desconto;
+    private double valor_final;
+
+     //SE FOR AFILIADO DESCONTO DE 60%
+    public double TipoAfiliado(double valor) {
+         desconto = (60 * valor) / 100;
+         valor_final = valor - desconto;   
+        
+        return valor_final;
+    }
+     //SE FOR ESTUDANTE DESCONTO DE 50%
+     public double TipoEstudante(double valor) {
+        desconto = (50 * valor) / 100;
+        valor_final = valor - desconto;     
+       return valor_final;
+   }
+     //SE FOR DEFICIENTE DESCONTO DE 70%
+     public double TipoDeficiente(double valor) {
+        desconto = (70 * valor) / 100;
+        valor_final = valor - desconto;     
+       return valor_final;
+   }
+     //SE FOR PUBLICO GERAL NÃO POSSUI DESCONTO 
+     public double TipoGeral(double valor) {
+        valor_final = valor;
+       return valor_final;
+   }
+}
---
+++ VendasDeIngresso/src/VendaDeIngressoTeste.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VendaDeIngressoTeste {
+
+    VendaDeIngresso Compra = new VendaDeIngresso();
+
+    double delta = 0.0;
+
+    //TESTE PARA AFILIADO
+    @Test
+    public void TipoAfiliado(){
+        Assert.assertEquals(40, Compra.TipoAfiliado(100), delta);
+    }
+    //TESTE PARA ESTUDANTE
+    @Test
+    public void TipoEstudante(){
+        Assert.assertEquals(50, Compra.TipoEstudante(100), delta);
+    }
+    //TESTE PARA DEFICIENTE
+    @Test
+    public void TipoDeficiente(){
+        Assert.assertEquals(30, Compra.TipoDeficiente(100), delta);
+    }
+    //TESTE PARA DEFICIENTE
+    @Test
+    public void TipoGeral(){
+        Assert.assertEquals(100, Compra.TipoGeral(100), delta);
+    }
+}
